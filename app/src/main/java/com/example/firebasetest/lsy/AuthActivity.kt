ac3f0aa9438354ac9f59cdd2da4ec4174d935f74
lsy3709 <lsy3709@naver.com>
2023-11-20 09:52:51 +0900

    1120 1교시 파이어 인증 뷰 간단히 구성하기

--- app/src/main/java/com/example/firebasetest/lsy/AuthActivity.kt
+++ app/src/main/java/com/example/firebasetest/lsy/AuthActivity.kt
@@ -1,16 +1,32 @@
 package com.example.firebasetest.lsy
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.example.firebasetest.lsy.databinding.ActivityAuthBinding
 
 class AuthActivity : AppCompatActivity() {
+    lateinit var binding : ActivityAuthBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_auth)
+        binding = ActivityAuthBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         // 다음 시간에, 이메일, 구글 인증, 후
         // 인증 후 2번째 페이지 접근 확인하고,
         // 그리고 나서, 2번째 페이지 부분을 아래의 기능으로 구성.
         // 스토어, 스토리지 에 접근 및 이용하는 부분 보여 주기.
+
+        //MyApplication 인증 함수를 이용해서,
+    // 인증이 되면, 특정 뷰로 인증됨을 확인
+        if(MyApplication.checkAuth()){
+            Log.d("lsy","로그인 인증이 됨")
+        } else {
+            Log.d("lsy","로그인 인증이 안됨")
+        }
+
+     // onCreate
     }
+
+
 }
