6927e5f6034473530542677cca0286827c666745
lsy3709 <lsy3709@naver.com>
2023-11-20 12:43:42 +0900

    1120 4교시 파이어 스토리지 갤러리 선택된 사진 미리보기, 업로드 뷰 구성

--- app/src/main/java/com/example/firebasetest/lsy/AddActivity.kt
+++ app/src/main/java/com/example/firebasetest/lsy/AddActivity.kt
@@ -3,6 +3,8 @@ package com.example.firebasetest.lsy
 import android.os.Bundle
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.example.firebasetest.lsy.databinding.ActivityAddBinding
 
 class AddActivity : AppCompatActivity() {
@@ -29,7 +31,19 @@ class AddActivity : AppCompatActivity() {
             // 이미지 불러오는 라이브러리 glide 사용하기, 코루틴이 적용이되어서, 매우 빠름.
             // OOM(Out Of Memory 해결), gif 움직이는 사진도 가능.
             // Glide 설치하기. build.gradle
-
+            // with(this) this 현재 액티비티 가리킴. 대신해서.
+            // 1) applicationContext
+            // 2) getApplicationContext()
+            Glide
+                .with(getApplicationContext())
+                // 사진을 읽기.
+                .load(it.data?.data)
+            // 크기 지정 , 가로,세로
+                .apply(RequestOptions().override(250,200))
+            // 선택된 사진 크기 자동 조정
+                .centerCrop()
+                // 결과 뷰에 사진 넣기.
+                .into()
         }
     }
 
--- app/src/main/java/com/example/firebasetest/lsy/MyApplication.kt
+++ app/src/main/java/com/example/firebasetest/lsy/MyApplication.kt
@@ -3,7 +3,10 @@ package com.example.firebasetest.lsy
 import androidx.multidex.MultiDexApplication
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.ktx.storage
 
 
 class MyApplication : MultiDexApplication() {
@@ -20,6 +23,12 @@ class MyApplication : MultiDexApplication() {
         lateinit var auth: FirebaseAuth
         // 인증할 이메일
         var email : String? = null
+
+        // 이미지 저장소 , 인스턴스 도구
+        lateinit var storage: FirebaseStorage
+        // 파이어 스토어, 인스턴스 도구
+        lateinit var db : FirebaseFirestore
+
 
         // MyApplication.checkAuth() : 이렇게 클래스명. 함수및 특정 변수에 접근이 가능함.
         fun checkAuth():Boolean {
@@ -43,6 +52,8 @@ class MyApplication : MultiDexApplication() {
         super.onCreate()
         // 초기화를 함.
         auth = Firebase.auth
+        storage = Firebase.storage
+        db = FirebaseFirestore.getInstance()
 
     }
 
