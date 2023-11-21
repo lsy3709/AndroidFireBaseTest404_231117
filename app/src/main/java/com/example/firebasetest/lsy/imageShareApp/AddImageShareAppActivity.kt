f0993a5d9370157afd2e4cff6294db7599aec2ed
lsy3709 <lsy3709@naver.com>
2023-11-21 11:48:12 +0900

    1121 3교시 이미지 공유 앱, 글쓰기 작업중, 이미지 업로드 함수 구성.

--- app/src/main/java/com/example/firebasetest/lsy/imageShareApp/AddImageShareAppActivity.kt
+++ app/src/main/java/com/example/firebasetest/lsy/imageShareApp/AddImageShareAppActivity.kt
@@ -12,8 +12,10 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import com.example.firebasetest.lsy.MyApplication
 import com.example.firebasetest.lsy.R
 import com.example.firebasetest.lsy.databinding.ActivityAddImageShareAppBinding
+import java.io.File
 
 class AddImageShareAppActivity : AppCompatActivity() {
     lateinit var binding : ActivityAddImageShareAppBinding
@@ -84,15 +86,53 @@ class AddImageShareAppActivity : AppCompatActivity() {
     // 붙힌 메뉴 이벤트 처리하기.
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if(item.itemId === R.id.menu_add_gallery) {
+            // 갤러리 사진 선택후, 결과 이미지 뷰에 , 프리뷰 미리보기 구성.
             val intent = Intent(Intent.ACTION_PICK)
             intent.setDataAndType(
                 MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
                 "image/*"
             )
             requestLauncher.launch(intent)
+        } else if (item.itemId === R.id.menu_add_save) {
+            // 불러온 이미지와, 콘텐츠 내용 , 스토어, 스토리지 사용하기.
+
         }
         // 저장 구성, 인증은 메인으로 옮기기
         return super.onOptionsItemSelected(item)
+    }
+
+    // 스토어 글쓰기 함수,
+
+    // 스토리지에 업로드 함수.
+    // 변경되는 부분, 이미지 파일명 = 스토어에 자동 저장이되는 id로 ,
+    // docId , 스토어에서 자동생성된 문서번호 = 이미지 명 , 재사용.
+    private fun uploadImage(docId: String) {
+        // 스토리지 접근 도구 ,인스턴스
+        val storage = MyApplication.storage
+        // 스토리지에 저장할 인스턴스
+        val storageRef = storage.reference
+
+        // 임시 파일 명 중복 안되게 생성하는 랜덤 문자열.
+//        val uuid = UUID.randomUUID().toString()
+//            출처: https://yoon-dailylife.tistory.com/94 [알면 쓸모있는 개발 지식:티스토리]
+
+        // 이미지 저장될 위치 및 파일명
+        val imgRef = storageRef.child("AndroidImageShareApp/${docId}.jpg")
+
+        // 파일 불러오기. 갤러리에서 사진을 선택 했고, 또한, 해당 위치에 접근해서,
+        // 파일도 불러오기 가능함.
+        val file = Uri.fromFile(File(filePath))
+        // 파이어베이스 스토리지에 업로드하는 함수.
+        imgRef.putFile(file)
+            // 업로드 후, 수행할 콜백 함수 정의. 실패했을 경우 콜백함수 정의
+            .addOnCompleteListener{
+                Toast.makeText(this,"스토리지 업로드 완료",Toast.LENGTH_SHORT).show()
+                // 현재 액티비티 종료, 글쓰기 페이지 종료,-> 메인으로 가기.
+                finish()
+            }
+            .addOnFailureListener {
+                Toast.makeText(this,"스토리지 업로드 실패",Toast.LENGTH_SHORT).show()
+            }
     }
 
 
