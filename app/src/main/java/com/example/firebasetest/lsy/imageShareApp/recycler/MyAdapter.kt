611c79f05116bb2067147e2b0bcf1cbc90f2e360
lsy3709 <lsy3709@naver.com>
2023-11-21 09:36:56 +0900

    1121 1교시 이미지 공유 앱, 기본, 뼈대, 리사이클러 작업중2 , 목록 요소 뷰 구성

--- app/src/main/java/com/example/firebasetest/lsy/imageShareApp/recycler/MyAdapter.kt
+++ app/src/main/java/com/example/firebasetest/lsy/imageShareApp/recycler/MyAdapter.kt
@@ -1,5 +1,8 @@
 package com.example.firebasetest.lsy.imageShareApp.recycler
 
+//뷰홀더 설정.
+// 아이템 요소의 뷰를 설정하기.
+class MyViewHolder(val )
 class MyAdapter {
     // 목록 요소에, 데이터를 연결 시켜주는 역할.
     // 뷰홀더 있음.
