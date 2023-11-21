563d11366d32086cb1d57ea40def9aeb308e9c56
lsy3709 <lsy3709@naver.com>
2023-11-21 10:19:44 +0900

    1121 2교시 이미지 공유 앱, 메인 뷰 구성, 텍스트,리사이클러뷰,플로팅액션버튼

--- app/src/main/java/com/example/firebasetest/lsy/imageShareApp/MainImageShareAppActivity.kt
+++ app/src/main/java/com/example/firebasetest/lsy/imageShareApp/MainImageShareAppActivity.kt
@@ -2,7 +2,7 @@ package com.example.firebasetest.lsy.imageShareApp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.firebasetest.lsy.R
+import com.example.firebasetest.lsy.databinding.ActivityMainImageShareAppBinding
 
 // 스토어, 스토리지에서  데이터를 받아서, 리사이클러뷰 로 출력할 예정.
 // 인증, 구글인증, 이메일 , 패스워드 인증 재사용.
@@ -10,8 +10,14 @@ import com.example.firebasetest.lsy.R
 // 일단, 삭제한번 도전해보고,
 // 수정 도전해보기.
 class MainImageShareAppActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainImageShareAppBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_image_share_app)
-    }
+        binding = ActivityMainImageShareAppBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }// onCreate
+
+
 }
