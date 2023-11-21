7698ed917ef2d7d81ca7f47443efab8cee12e616
lsy3709 <lsy3709@naver.com>
2023-11-21 09:23:06 +0900

    1121 1교시 이미지 공유 앱, 기본, 뼈대, 리사이클러 작업중1

---
+++ app/src/main/java/com/example/firebasetest/lsy/imageShareApp/MainImageShareAppActivity.kt
@@ -0,0 +1,17 @@
+package com.example.firebasetest.lsy.imageShareApp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.firebasetest.lsy.R
+
+// 스토어, 스토리지에서  데이터를 받아서, 리사이클러뷰 로 출력할 예정.
+// 인증, 구글인증, 이메일 , 패스워드 인증 재사용.
+// 인증이 되어야 글쓰기 가능하게 하고,
+// 일단, 삭제한번 도전해보고,
+// 수정 도전해보기.
+class MainImageShareAppActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_image_share_app)
+    }
+}
---
+++ app/src/main/java/com/example/firebasetest/lsy/imageShareApp/recycler/MyAdapter.kt
@@ -0,0 +1,6 @@
+package com.example.firebasetest.lsy.imageShareApp.recycler
+
+class MyAdapter {
+    // 목록 요소에, 데이터를 연결 시켜주는 역할.
+    // 뷰홀더 있음.
+}
