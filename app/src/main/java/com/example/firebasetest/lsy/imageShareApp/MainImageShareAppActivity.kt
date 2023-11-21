91a55d4fedb572eaa2cb3b15e162db4ca9f36d06
lsy3709 <lsy3709@naver.com>
2023-11-21 10:33:12 +0900

    1121 2교시 이미지 공유 앱, 글쓰기 전에 인증 체크 후 성공 글쓰기 페이지, 실패 인증 해주세요. 구성

--- app/src/main/java/com/example/firebasetest/lsy/imageShareApp/MainImageShareAppActivity.kt
+++ app/src/main/java/com/example/firebasetest/lsy/imageShareApp/MainImageShareAppActivity.kt
@@ -1,7 +1,10 @@
 package com.example.firebasetest.lsy.imageShareApp
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.firebasetest.lsy.MyApplication
+import com.example.firebasetest.lsy.Utils.MyUtil
 import com.example.firebasetest.lsy.databinding.ActivityMainImageShareAppBinding
 
 // 스토어, 스토리지에서  데이터를 받아서, 리사이클러뷰 로 출력할 예정.
@@ -16,7 +19,18 @@ class MainImageShareAppActivity : AppCompatActivity() {
         binding = ActivityMainImageShareAppBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // 해당 메인 앱이 실행이 되면, 먼저, 현재 앱의 외부 스토리지에 접근 권한 체크를 하기.
+        MyUtil.checkPermission(this)
 
+        // 플로팅 액션 번튼 클릭시, 글쓰기 뷰로 이동하기 작업.
+        binding.addFabtn.setOnClickListener {
+            // 글쓰기 전에, 인증 여부 확인 하기.
+            if(MyApplication.checkAuth()) {
+                // 글쓰기 페이지 이동 -> AddImageShareApp
+            } else {
+                Toast.makeText(this,"인증 후 글쓰기 해주세요",Toast.LENGTH_SHORT).show()
+            }
+        }
 
 
     }// onCreate
