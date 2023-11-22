13c96d9fd871eae0c350dd9e442fbcba8cffdab2
lsy3709 <lsy3709@naver.com>
2023-11-22 10:20:43 +0900

    1122 2교시 FCM 사전 준비 작업.

---
+++ app/src/main/java/com/example/firebasetest/lsy/fcmTest/MyFirebaseFcmService.kt
@@ -0,0 +1,12 @@
+package com.example.firebasetest.lsy.fcmTest
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class MyFirebaseFcmService : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+}
