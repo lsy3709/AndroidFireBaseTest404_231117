af5060e58fd244aa76c63f0f4cbc476f3ce3d1ac
lsy3709 <lsy3709@naver.com>
2023-11-22 10:29:22 +0900

    1122 2교시 FCM 사전 준비 작업3

--- app/src/main/java/com/example/firebasetest/lsy/fcmTest/MyFirebaseFcmService.kt
+++ app/src/main/java/com/example/firebasetest/lsy/fcmTest/MyFirebaseFcmService.kt
@@ -1,12 +1,22 @@
 package com.example.firebasetest.lsy.fcmTest
 
-import android.app.Service
-import android.content.Intent
-import android.os.IBinder
+// 설정1, 부모 클래스 변경.
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 
-class MyFirebaseFcmService : Service() {
+class MyFirebaseFcmService : FirebaseMessagingService() {
 
-    override fun onBind(intent: Intent): IBinder {
-        TODO("Return the communication channel to the service.")
+    // 토큰을 새로 발급을 받는 함수
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+        Log.d("lsy","fcm 토큰 출력 : ${token}")
+    }
+
+    // 메세지를 전달 받는 함수
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+        Log.d("lsy","fcm 메세지 내용들 message.notification : ${message.notification}")
+        Log.d("lsy","fcm 메세지 내용들 message.data :  ${message.data}")
     }
 }
